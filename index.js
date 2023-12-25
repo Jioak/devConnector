5ea821619f3ea4daf27a11fc10e9da4ae3fbcdd2
Jioak <gakhil399@gmail.com>
2023-12-25 15:52:28 +0530

    b8

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express');
 const connectDB=require('./config/db')
 const dotenv = require("dotenv")
 // const auth=require('./middleware/auth')
-var cors = require('cors')
+const cors = require('cors')
 
 dotenv.config();
 
